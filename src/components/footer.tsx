92d626e48b87656e9474ead179e7cadcb242e492
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-08-04 10:12:14 +0300

    Fix: Fix Footer Appearance on Small Devices

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,9 +5,9 @@ import { footerItems } from "~/config/footer";
 
 export function Footer() {
   return (
-    <div className="mx-auto mt-12 max-w-2xl border-t border-gray-300 py-4 text-sm dark:border-gray-500">
-      <div className="flex items-center justify-between">
-        <span className=" text-gray-400 dark:text-gray-500">
+    <div className="mx-auto mt-12 max-w-2xl border-t border-gray-300 pb-4 pt-2 text-sm dark:border-gray-500 sm:py-2">
+      <div className="flex items-center">
+        <span className="text-gray-400 dark:text-gray-500">
           Copyright © {new Date().getFullYear()} Peter Kibuchi.
         </span>
 
@@ -20,7 +20,8 @@ export function Footer() {
               referrerPolicy="no-referrer"
               className={buttonVariants({ variant: "ghost" })}
             >
-              {item.title}
+              <item.icon className="h-4 w-4" />
+              <span className="sr-only">{item.title}</span>
             </Link>
           ))}
         </div>
--- src/config/footer.ts
+++ src/config/footer.ts
@@ -1,16 +1,24 @@
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { type IconProps } from "@radix-ui/react-icons/dist/types";
+
 type FooterItem = {
   title: string;
   href: string;
   disabled?: boolean;
+  icon: React.ForwardRefExoticComponent<
+    IconProps & React.RefAttributes<SVGSVGElement>
+  >;
 };
 
 export const footerItems: FooterItem[] = [
   {
     title: "GitHub",
     href: "https://github.com/peterkibuchi",
+    icon: GitHubLogoIcon,
   },
   {
     title: "LinkedIn",
     href: "https://linkedin.com/in/peterkibuchi",
+    icon: LinkedInLogoIcon,
   },
 ];
