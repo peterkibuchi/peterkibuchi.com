0efa4ca9c13331fd235753b296095e7fd21c0045
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-04-26 08:07:02 +0300

    Remove unnecessary `reactStrictMode` & `swcMinify` properties as they are on by default

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
